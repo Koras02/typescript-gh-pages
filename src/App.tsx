eb6e334ce7c03b3be85876be491f59c3f0fd61aa
Koras02 <wjdgh0727@gmail.com>
2021-10-21 20:57:37 +0900

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+type CoffeeCup = {
+  shots: number;
+  hasmilk: boolean;
+};
+
+class CoffeeMaker {
+  static BEANS_GRAMM_PER_SHOT = 7;
+
+  coffeeBeans = 0;
+
+  constructor(coffeeBeans: number) {
+    this.coffeeBeans = coffeeBeans;
+  }
+
+  static makeMarchine(coffeeBeans: number): CoffeeMaker {
+    return new CoffeeMaker(coffeeBeans);
+  }
+
+  makeCoffee(shots: number): CoffeeCup {
+    this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+    return {
+      shots,
+      hasmilk: false,
+    };
+  }
+}
+
+const maker = new CoffeeMaker(32);
+console.log(maker);
+
+const maker2 = new CoffeeMaker(14);
+console.log(maker2);
+
+// maker2.makeCoffee();
+
+const maker3 = CoffeeMaker.makeMarchine(3);
+console.log(maker3);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>현재 커피 잔의 개수는 {CoffeeMaker.BEANS_GRAMM_PER_SHOT} 입니다.</div>
   );
 }
 
